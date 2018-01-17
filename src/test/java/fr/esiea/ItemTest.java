d80e1eb710546fde96703b338e61912dce1bb3b2
Stalker <arnaud.vadez@gmail.com>
2018-01-17 09:35:01 +0100

    test de l'affichage du nom (toString) d'un Item.

--- src/test/java/fr/esiea/ItemTest.java
+++ src/test/java/fr/esiea/ItemTest.java
@@ -1,5 +1,6 @@
 package fr.esiea;
 
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,12 +9,18 @@ public class ItemTest {
 
     @Test
     public void creationTest() {
-        Item testItem = new Item("creationTestItem", 10, 10);
+        Item creationTestItem = new Item("creationTestItem", 10, 10);
         SoftAssertions softAssert = new SoftAssertions();
 
-        softAssert.assertThat(testItem.name).as("Test object name").isEqualTo("creationTestItem");
-        softAssert.assertThat(testItem.sellIn).as("Test object sellIn").isEqualTo(10);
-        softAssert.assertThat(testItem.quality).as("Test object quality").isEqualTo(10);
+        softAssert.assertThat(creationTestItem.name).as("Test object name").isEqualTo("creationTestItem");
+        softAssert.assertThat(creationTestItem.sellIn).as("Test object sellIn").isEqualTo(10);
+        softAssert.assertThat(creationTestItem.quality).as("Test object quality").isEqualTo(10);
         softAssert.assertAll();
+    }
+
+    @Test
+    public void toStringTest() {
+        Item stringTestItem = new Item("toStringTestItem", 10, 10);
+        Assertions.assertThat(stringTestItem.toString()).as("Display of an object").isEqualTo("toStringTestItem, 10, 10");
     }
 }
