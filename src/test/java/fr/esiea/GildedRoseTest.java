20e8bd594b29ca28178ffc475cd3bca729c96bc3
vbonneau <victor.bonneau27@gmail.com>
2018-01-28 22:47:05 +0100

    Sulfuras SellIn and Backstage Quality

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -33,11 +33,29 @@ public class GildedRoseTest {
     }
 
     @Test
+    public void UpdateQualityBackstageQuality(){
+        Item defaultItem = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10);
+        Item[] items = new Item[]{defaultItem};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assertions.assertThat(defaultItem.quality).as("qualité de Backstage ").isEqualTo(13);
+    }
+
+    @Test
     public void UpdateQualityDefaultSellIn(){
         Item defaultItem = new Item("azerty", 5, 10);
         Item[] items = new Item[]{defaultItem};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         Assertions.assertThat(defaultItem.sellIn).as("nombre de jours pour vendre le produit").isEqualTo(4);
+    }
+
+    @Test
+    public void UpdateQualitySulfurasSellIn(){
+        Item defaultItem = new Item("Sulfuras, Hand of Ragnaros", 5, 10);
+        Item[] items = new Item[]{defaultItem};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assertions.assertThat(defaultItem.sellIn).as("nombre de jours pour vendre Sulfuras, Hand of Ragnaros").isEqualTo(5);
     }
 }
