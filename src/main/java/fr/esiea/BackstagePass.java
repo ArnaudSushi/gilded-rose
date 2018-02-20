643b21cb077922fdd6f66a1825505f58d25ab202
Stalker <arnaud.vadez@gmail.com>
2018-02-20 14:34:22 +0100

    Modification of the GildedRose class, updateQuality rewritten

--- src/main/java/fr/esiea/BackstagePass.java
+++ src/main/java/fr/esiea/BackstagePass.java
@@ -1,12 +1,25 @@
 package fr.esiea;
 
 public class BackstagePass extends Item {
+
     public BackstagePass(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void updateQuality() {
-
+        /*
+        quality++;
+        if (sellIn <= 10) {
+            quality++;
+        }
+        if (sellIn <= 5) {
+            quality++;
+        }
+        if (sellIn < 0) {
+            quality = 0;
+        }*/
+        int qualityEvolution = ((10/sellIn) + 1);
+        quality += qualityEvolution;
     }
 }
